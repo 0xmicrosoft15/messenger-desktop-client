telegramdesktop/tdesktop

commit b08732cf28b3fe8c16c9afc14fa4fef6995d2e80

Move MediaView from MainWindow to Messenger.

Telegram/SourceFiles/messenger.cpp
@@ -35,6 +35,7 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 #include "observer_peer.h"
 #include "storage/file_upload.h"
 #include "mainwidget.h"
+#include "mediaview.h"
 #include "mtproto/dc_options.h"
 #include "mtproto/mtp_instance.h"
 #include "media/player/media_player_instance.h"
@@ -129,6 +130,9 @@ Messenger::Messenger() : QObject()
 	_window->createWinId();
 	_window->init();
 
+	_mediaView = std::make_unique<MediaView>();
+
+	QCoreApplication::instance()->installEventFilter(this);
 	Sandbox::connect(SIGNAL(applicationStateChanged(Qt::ApplicationState)), this, SLOT(onAppStateChanged(Qt::ApplicationState)));
 
 	DEBUG_LOG(("Application Info: window created..."));
@@ -178,6 +182,97 @@ Messenger::Messenger() : QObject()
 			LOG(("Shortcuts Error: %1").arg(*i));
 		}
 	}
+}
+
+bool Messenger::hideMediaView() {
+	if (_mediaView && !_mediaView->isHidden()) {
+		_mediaView->hide();
+		_window->reActivateWindow();
+		return true;
+	}
+	return false;
+}
+
+void Messenger::showPhoto(gsl::not_null<const PhotoOpenClickHandler*> link, HistoryItem *item) {
+	return (!item && link->peer())
+		? showPhoto(link->photo(), link->peer())
+		: showPhoto(link->photo(), item);
+}
+
+void Messenger::showPhoto(gsl::not_null<PhotoData*> photo, HistoryItem *item) {
+	if (_mediaView->isHidden()) Ui::hideLayer(true);
+	_mediaView->showPhoto(photo, item);
+	_mediaView->activateWindow();
+	_mediaView->setFocus();
+}
+
+void Messenger::showPhoto(gsl::not_null<PhotoData*> photo, PeerData *peer) {
+	if (_mediaView->isHidden()) Ui::hideLayer(true);
+	_mediaView->showPhoto(photo, peer);
+	_mediaView->activateWindow();
+	_mediaView->setFocus();
+}
+
+void Messenger::showDocument(gsl::not_null<DocumentData*> document, HistoryItem *item) {
+	if (cUseExternalVideoPlayer() && document->isVideo()) {
+		QDesktopServices::openUrl(QUrl("file:///" + document->location(false).fname));
+	} else {
+		if (_mediaView->isHidden()) Ui::hideLayer(true);
+		_mediaView->showDocument(document, item);
+		_mediaView->activateWindow();
+		_mediaView->setFocus();
+	}
+}
+
+PeerData *Messenger::ui_getPeerForMouseAction() {
+	if (_mediaView && !_mediaView->isHidden()) {
+		return _mediaView->ui_getPeerForMouseAction();
+	} else if (auto main = App::main()) {
+		return main->ui_getPeerForMouseAction();
+	}
+	return nullptr;
+}
+
+bool Messenger::eventFilter(QObject *object, QEvent *e) {
+	switch (e->type()) {
+	case QEvent::KeyPress:
+	case QEvent::MouseButtonPress:
+	case QEvent::TouchBegin:
+	case QEvent::Wheel: {
+		psUserActionDone();
+	} break;
+
+	case QEvent::ShortcutOverride: {
+		// handle shortcuts ourselves
+		return true;
+	} break;
+
+	case QEvent::Shortcut: {
+		DEBUG_LOG(("Shortcut event caught: %1").arg(static_cast<QShortcutEvent*>(e)->key().toString()));
+		if (Shortcuts::launch(static_cast<QShortcutEvent*>(e)->shortcutId())) {
+			return true;
+		}
+	} break;
+
+	case QEvent::ApplicationActivate: {
+		if (object == QCoreApplication::instance()) {
+			psUserActionDone();
+		}
+	} break;
+
+	case QEvent::FileOpen: {
+		if (object == QCoreApplication::instance()) {
+			auto url = QString::fromUtf8(static_cast<QFileOpenEvent*>(e)->url().toEncoded().trimmed());
+			if (url.startsWith(qstr("tg://"), Qt::CaseInsensitive)) {
+				cSetStartUrl(url.mid(0, 8192));
+				checkStartUrl();
+			}
+			_window->activate();
+		}
+	} break;
+	}
+
+	return QObject::eventFilter(object, e);
 }
 
 void Messenger::setMtpMainDcId(MTP::DcId mainDcId) {
@@ -616,6 +711,8 @@ void Messenger::authSessionDestroy() {
 	_private->storedAuthSession.reset();
 	_private->authSessionUserId = 0;
 	authSessionChanged().notify(true);
+
+	loggedOut();
 }
 
 void Messenger::setInternalLinkDomain(const QString &domain) const {
@@ -800,6 +897,7 @@ Messenger::~Messenger() {
 	Expects(SingleInstance == this);
 
 	_window.reset();
+	_mediaView.reset();
 
 	// Some MTP requests can be cancelled from data clearing.
 	App::clearHistories();
@@ -832,8 +930,28 @@ Messenger::~Messenger() {
 	SingleInstance = nullptr;
 }
 
-MainWindow *Messenger::mainWindow() {
+MainWindow *Messenger::getActiveWindow() {
 	return _window.get();
+}
+
+QWidget *Messenger::getFileDialogParent() {
+	return (_mediaView && _mediaView->isVisible()) ? (QWidget*)_mediaView.get() : (QWidget*)getActiveWindow();
+}
+
+void Messenger::checkMediaViewActivation() {
+	if (_mediaView && !_mediaView->isHidden()) {
+		_mediaView->activateWindow();
+		Sandbox::setActiveWindow(_mediaView.get());
+		_mediaView->setFocus();
+	}
+}
+
+void Messenger::loggedOut() {
+	if (_mediaView) {
+		hideMediaView();
+		_mediaView->rpcClear();
+		_mediaView->clearData();
+	}
 }
 
 QPoint Messenger::getPointForCallPanelCenter() const {

Telegram/SourceFiles/app.cpp
@@ -160,7 +160,7 @@ namespace App {
 
 	MainWindow *wnd() {
 		if (auto instance = Messenger::InstancePointer()) {
-			return instance->mainWindow();
+			return instance->getActiveWindow();
 		}
 		return nullptr;
 	}

Telegram/SourceFiles/auth_session.h
@@ -222,6 +222,9 @@ public:
 	void checkAutoLock();
 	void checkAutoLockIn(TimeMs time);
 
+	base::Observable<DocumentData*> documentUpdated;
+	base::Observable<std::pair<HistoryItem*, MsgId>> messageIdChanging;
+
 	~AuthSession();
 
 private:

Telegram/SourceFiles/core/file_utilities.cpp
@@ -24,6 +24,7 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 #include "storage/localstorage.h"
 #include "platform/platform_file_utilities.h"
 #include "base/task_queue.h"
+#include "messenger.h"
 
 bool filedialogGetSaveFile(QString &file, const QString &caption, const QString &filter, const QString &initialPath) {
 	QStringList files;
@@ -220,7 +221,7 @@ bool GetDefault(QStringList &files, QByteArray &remoteContent, const QString &ca
 	}
 	QString file;
 	if (type == Type::ReadFiles) {
-		files = QFileDialog::getOpenFileNames(App::wnd() ? App::wnd()->filedialogParent() : 0, caption, startFile, filter);
+		files = QFileDialog::getOpenFileNames(Messenger::Instance().getFileDialogParent(), caption, startFile, filter);
 		QString path = files.isEmpty() ? QString() : QFileInfo(files.back()).absoluteDir().absolutePath();
 		if (!path.isEmpty() && path != cDialogLastPath()) {
 			cSetDialogLastPath(path);
@@ -228,11 +229,11 @@ bool GetDefault(QStringList &files, QByteArray &remoteContent, const QString &ca
 		}
 		return !files.isEmpty();
     } else if (type == Type::ReadFolder) {
-		file = QFileDialog::getExistingDirectory(App::wnd() ? App::wnd()->filedialogParent() : 0, caption, startFile);
+		file = QFileDialog::getExistingDirectory(Messenger::Instance().getFileDialogParent(), caption, startFile);
     } else if (type == Type::WriteFile) {
-		file = QFileDialog::getSaveFileName(App::wnd() ? App::wnd()->filedialogParent() : 0, caption, startFile, filter);
+		file = QFileDialog::getSaveFileName(Messenger::Instance().getFileDialogParent(), caption, startFile, filter);
     } else {
-		file = QFileDialog::getOpenFileName(App::wnd() ? App::wnd()->filedialogParent() : 0, caption, startFile, filter);
+		file = QFileDialog::getOpenFileName(Messenger::Instance().getFileDialogParent(), caption, startFile, filter);
     }
     if (file.isEmpty()) {
         files = QStringList();

Telegram/SourceFiles/facades.cpp
@@ -241,11 +241,6 @@ bool isLayerShown() {
 	return false;
 }
 
-bool isMediaViewShown() {
-	if (auto w = App::wnd()) return w->ui_isMediaViewShown();
-	return false;
-}
-
 void repaintHistoryItem(gsl::not_null<const HistoryItem*> item) {
 	if (auto main = App::main()) {
 		main->ui_repaintHistoryItem(item);
@@ -282,10 +277,7 @@ void showPeerHistoryAsync(const PeerId &peer, MsgId msgId, ShowWay way) {
 }
 
 PeerData *getPeerForMouseAction() {
-	if (auto w = App::wnd()) {
-		return w->ui_getPeerForMouseAction();
-	}
-	return nullptr;
+	return Messenger::Instance().ui_getPeerForMouseAction();
 }
 
 bool hideWindowNoQuit() {

Telegram/SourceFiles/facades.h
@@ -103,7 +103,6 @@ QPointer<BoxType> show(object_ptr<BoxType> content, ShowLayerOptions options = C
 void hideLayer(bool fast = false);
 void hideSettingsAndLayer(bool fast = false);
 bool isLayerShown();
-bool isMediaViewShown();
 
 void repaintHistoryItem(gsl::not_null<const HistoryItem*> item);
 void autoplayMediaInlineAsync(const FullMsgId &msgId);

Telegram/SourceFiles/history/history_admin_log_inner.cpp
@@ -29,6 +29,7 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 #include "chat_helpers/message_field.h"
 #include "mainwindow.h"
 #include "mainwidget.h"
+#include "messenger.h"
 #include "apiwrap.h"
 #include "window/window_controller.h"
 #include "auth_session.h"
@@ -986,7 +987,7 @@ void InnerWidget::openContextGif() {
 	if (auto item = App::contextItem()) {
 		if (auto media = item->getMedia()) {
 			if (auto document = media->getDocument()) {
-				_controller->window()->showDocument(document, item);
+				Messenger::Instance().showDocument(document, item);
 			}
 		}
 	}

Telegram/SourceFiles/history/history_inner_widget.cpp
@@ -33,6 +33,7 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 #include "mainwindow.h"
 #include "mainwidget.h"
 #include "auth_session.h"
+#include "messenger.h"
 #include "apiwrap.h"
 #include "lang/lang_keys.h"
 
@@ -1463,7 +1464,7 @@ void HistoryInner::openContextGif() {
 	if (auto item = App::contextItem()) {
 		if (auto media = item->getMedia()) {
 			if (auto document = media->getDocument()) {
-				_controller->window()->showDocument(document, item);
+				Messenger::Instance().showDocument(document, item);
 			}
 		}
 	}

0xmicrosoft15/messenger-desktop-client

commit 4f6774146691dcaa8384436a6c949aa9d71f3da0

Fix window size reset on every launch.
Regression was introduced in b08732cf28.

In MediaView constructor a call to setWindowState(WindowFullScreen)
invokes QWindowSystemInterface::flushWindowSystemEvents() which then
delivers some delayed system window resize event for the main window,
resetting its size to something default.

Telegram/SourceFiles/messenger.cpp
@@ -130,7 +130,9 @@ Messenger::Messenger() : QObject()
 	_window->createWinId();
 	_window->init();
 
+	auto currentGeometry = _window->geometry();
 	_mediaView = std::make_unique<MediaView>();
+	_window->setGeometry(currentGeometry);
 
 	QCoreApplication::instance()->installEventFilter(this);
 	Sandbox::connect(SIGNAL(applicationStateChanged(Qt::ApplicationState)), this, SLOT(onAppStateChanged(Qt::ApplicationState)));