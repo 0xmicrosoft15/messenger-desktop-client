telegramdesktop/tdesktop

commit c1a0dad2b76abca2e1ae0077e178b885e5927486

Use QOpenGLWidget for macOS media viewer overlay.
Also move mediaview to media/view/media_view_overlay_widget.

Telegram/SourceFiles/media/view/media_view_overlay_widget.h
@@ -7,23 +7,12 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 */
 #pragma once
 
+#include "ui/rp_widget.h"
 #include "ui/widgets/dropdown_menu.h"
 #include "ui/effects/radial_animation.h"
 #include "data/data_shared_media.h"
 #include "data/data_user_photos.h"
 #include "data/data_web_page.h"
-
-namespace Media {
-namespace Player {
-struct TrackState;
-} // namespace Player
-namespace Clip {
-class Controller;
-} // namespace Clip
-namespace View {
-class GroupThumbs;
-} // namespace View
-} // namespace Media
 
 namespace Ui {
 class PopupMenu;
@@ -41,13 +30,29 @@ namespace Notify {
 struct PeerUpdate;
 } // namespace Notify
 
-class MediaView : public TWidget, private base::Subscriber, public ClickHandlerHost {
+
+namespace Media {
+namespace Player {
+struct TrackState;
+} // namespace Player
+namespace Clip {
+class Controller;
+} // namespace Clip
+namespace View {
+
+class GroupThumbs;
+
+#if defined Q_OS_MAC && !defined OS_MAC_OLD
+using OverlayParent = Ui::RpWidgetWrap<QOpenGLWidget>;
+#else // Q_OS_MAC && !OS_MAC_OLD
+using OverlayParent = Ui::RpWidget;
+#endif // Q_OS_MAC && !OS_MAC_OLD
+
+class OverlayWidget final : public OverlayParent, private base::Subscriber, public ClickHandlerHost {
 	Q_OBJECT
 
 public:
-	MediaView();
-
-	void setVisible(bool visible) override;
+	OverlayWidget();
 
 	void showPhoto(not_null<PhotoData*> photo, HistoryItem *context);
 	void showPhoto(not_null<PhotoData*> photo, not_null<PeerData*> context);
@@ -70,26 +75,11 @@ public:
 
 	void clearData();
 
-	~MediaView();
+	~OverlayWidget();
 
 	// ClickHandlerHost interface
 	void clickHandlerActiveChanged(const ClickHandlerPtr &p, bool active) override;
 	void clickHandlerPressedChanged(const ClickHandlerPtr &p, bool pressed) override;
-
-protected:
-	void paintEvent(QPaintEvent *e) override;
-
-	void keyPressEvent(QKeyEvent *e) override;
-	void wheelEvent(QWheelEvent *e) override;
-	void mousePressEvent(QMouseEvent *e) override;
-	void mouseDoubleClickEvent(QMouseEvent *e) override;
-	void mouseMoveEvent(QMouseEvent *e) override;
-	void mouseReleaseEvent(QMouseEvent *e) override;
-	void contextMenuEvent(QContextMenuEvent *e) override;
-	void touchEvent(QTouchEvent *e);
-
-	bool event(QEvent *e) override;
-	bool eventFilter(QObject *obj, QEvent *e) override;
 
 private slots:
 	void onHideControls(bool force = false);
@@ -135,17 +125,34 @@ private:
 		OverIcon,
 		OverVideo,
 	};
-
-	void updateOver(QPoint mpos);
-	void moveToScreen();
-	bool moveToNext(int delta);
-	void preloadData(int delta);
 	struct Entity {
 		base::optional_variant<
 			not_null<PhotoData*>,
 			not_null<DocumentData*>> data;
 		HistoryItem *item;
 	};
+
+	void paintEvent(QPaintEvent *e) override;
+
+	void keyPressEvent(QKeyEvent *e) override;
+	void wheelEvent(QWheelEvent *e) override;
+	void mousePressEvent(QMouseEvent *e) override;
+	void mouseDoubleClickEvent(QMouseEvent *e) override;
+	void mouseMoveEvent(QMouseEvent *e) override;
+	void mouseReleaseEvent(QMouseEvent *e) override;
+	void contextMenuEvent(QContextMenuEvent *e) override;
+	void touchEvent(QTouchEvent *e);
+
+	bool eventHook(QEvent *e) override;
+	bool eventFilter(QObject *obj, QEvent *e) override;
+
+	void setVisibleHook(bool visible) override;
+
+	void updateOver(QPoint mpos);
+	void moveToScreen();
+	bool moveToNext(int delta);
+	void preloadData(int delta);
+
 	Entity entityForUserPhotos(int index) const;
 	Entity entityForSharedMedia(int index) const;
 	Entity entityForCollage(int index) const;
@@ -411,3 +418,6 @@ private:
 	object_ptr<Ui::RoundButton> _themeCancel = { nullptr };
 
 };
+
+} // namespace View
+} // namespace Media

Telegram/SourceFiles/core/application.cpp
@@ -32,7 +32,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "observer_peer.h"
 #include "storage/storage_databases.h"
 #include "mainwidget.h"
-#include "mediaview.h"
+#include "media/view/media_view_overlay_widget.h"
 #include "mtproto/dc_options.h"
 #include "mtproto/mtp_instance.h"
 #include "media/player/media_player_instance.h"
@@ -129,7 +129,7 @@ void Application::run() {
 	_window->init();
 
 	auto currentGeometry = _window->geometry();
-	_mediaView = std::make_unique<MediaView>();
+	_mediaView = std::make_unique<Media::View::OverlayWidget>();
 	_window->setGeometry(currentGeometry);
 
 	QCoreApplication::instance()->installEventFilter(this);

Telegram/SourceFiles/core/application.h
@@ -16,7 +16,6 @@ class AuthSessionSettings;
 class MainWidget;
 class FileUploader;
 class Translator;
-class MediaView;
 class BoxContent;
 
 namespace Storage {
@@ -43,6 +42,9 @@ namespace Media {
 namespace Audio {
 class Instance;
 } // namespace Audio
+namespace View {
+class OverlayWidget;
+} // namespace View
 } // namespace Media
 
 namespace Lang {
@@ -81,7 +83,7 @@ public:
 		return &_globalShortcutParent;
 	}
 
-	// MediaView interface.
+	// Media view interface.
 	void checkMediaViewActivation();
 	bool hideMediaView();
 	void showPhoto(not_null<const PhotoOpenClickHandler*> link);
@@ -244,7 +246,7 @@ private:
 
 	std::unique_ptr<Storage::Databases> _databases;
 	std::unique_ptr<MainWindow> _window;
-	std::unique_ptr<MediaView> _mediaView;
+	std::unique_ptr<Media::View::OverlayWidget> _mediaView;
 	std::unique_ptr<Lang::Instance> _langpack;
 	std::unique_ptr<Lang::CloudManager> _langCloudManager;
 	std::unique_ptr<Lang::Translator> _translator;

Telegram/SourceFiles/mainwindow.cpp
@@ -31,7 +31,6 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "boxes/add_contact_box.h"
 #include "boxes/connection_box.h"
 #include "observer_peer.h"
-#include "mediaview.h"
 #include "storage/localstorage.h"
 #include "apiwrap.h"
 #include "settings/settings_intro.h"

Telegram/SourceFiles/media/view/media_clip_controller.cpp
@@ -232,7 +232,7 @@ void Controller::paintEvent(QPaintEvent *e) {
 }
 
 void Controller::mousePressEvent(QMouseEvent *e) {
-	e->accept(); // Don't pass event to the MediaView.
+	e->accept(); // Don't pass event to the Media::View::OverlayWidget.
 }
 
 Controller::~Controller() = default;

Telegram/SourceFiles/window/main_window.cpp
@@ -19,7 +19,6 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "core/sandbox.h"
 #include "lang/lang_keys.h"
 #include "data/data_session.h"
-#include "mediaview.h"
 #include "auth_session.h"
 #include "apiwrap.h"
 #include "mainwindow.h"

Telegram/SourceFiles/window/main_window.h
@@ -12,7 +12,6 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "base/timer.h"
 
 class BoxContent;
-class MediaView;
 
 namespace Window {
 

0xmicrosoft15/messenger-desktop-client

commit 06367ef4c40c110206166af3659db08d8dd86e9d

Version 1.5.15: Fix macOS media viewer overlay.
Regression was introduced in c1a0dad2b7.

Telegram/SourceFiles/media/view/media_view_overlay_widget.cpp
@@ -3107,7 +3107,9 @@ void OverlayWidget::touchEvent(QTouchEvent *e) {
 }
 
 bool OverlayWidget::eventHook(QEvent *e) {
-	if (e->type() == QEvent::TouchBegin || e->type() == QEvent::TouchUpdate || e->type() == QEvent::TouchEnd || e->type() == QEvent::TouchCancel) {
+	if (e->type() == QEvent::UpdateRequest) {
+		_wasRepainted = true;
+	} else if (e->type() == QEvent::TouchBegin || e->type() == QEvent::TouchUpdate || e->type() == QEvent::TouchEnd || e->type() == QEvent::TouchCancel) {
 		QTouchEvent *ev = static_cast<QTouchEvent*>(e);
 		if (ev->device()->type() == QTouchDevice::TouchScreen) {
 			if (ev->type() != QEvent::TouchBegin || ev->touchPoints().isEmpty() || !childAt(mapFromGlobal(ev->touchPoints().cbegin()->screenPos().toPoint()))) {
@@ -3175,6 +3177,25 @@ void OverlayWidget::setVisibleHook(bool visible) {
 		a_cOpacity = anim::value(1, 1);
 		_groupThumbs = nullptr;
 		_groupThumbsRect = QRect();
+#if defined Q_OS_MAC && !defined MAC_OS_OLD
+		// QOpenGLWidget can't properly destroy a child widget if
+		// it is hidden exactly after that, so it must be repainted
+		// before it is hidden without the child widget.
+		if (!isHidden() && _clipController) {
+			_clipController.destroy();
+			_wasRepainted = false;
+			repaint();
+			if (!_wasRepainted) {
+				// Qt has some optimization to prevent too frequent repaints.
+				// If the previous repaint was less than 1/60 second it silently
+				// converts repaint() call to an update() call. But we have to
+				// repaint right now, before hide(), with _clipController destroyed.
+				auto event = QEvent(QEvent::UpdateRequest);
+				QApplication::sendEvent(this, &event);
+			}
+		}
+#endif // Q_OS_MAC && !MAC_OS_OLD
+
 	}
 	OverlayParent::setVisibleHook(visible);
 	if (visible) {

Telegram/SourceFiles/media/view/media_view_overlay_widget.h
@@ -417,6 +417,8 @@ private:
 	object_ptr<Ui::RoundButton> _themeApply = { nullptr };
 	object_ptr<Ui::RoundButton> _themeCancel = { nullptr };
 
+	bool _wasRepainted = false;
+
 };
 
 } // namespace View