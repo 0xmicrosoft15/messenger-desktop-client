telegramdesktop/tdesktop

commit db5d599052a2afc6b30c2dce2ce07029dc4ef153

Don't resolve the actual QImage on paste check.

Telegram/SourceFiles/history/history_widget.cpp
@@ -4467,20 +4467,15 @@ bool HistoryWidget::confirmSendingFiles(
 bool HistoryWidget::canSendFiles(not_null<const QMimeData*> data) const {
 	if (!canWriteMessage()) {
 		return false;
-	}
-	if (const auto urls = data->urls(); !urls.empty()) {
+	} else if (const auto urls = data->urls(); !urls.empty()) {
 		if (ranges::find_if(
 			urls,
 			[](const QUrl &url) { return !url.isLocalFile(); }
 		) == urls.end()) {
 			return true;
 		}
-	}
-	if (data->hasImage()) {
-		const auto image = qvariant_cast<QImage>(data->imageData());
-		if (!image.isNull()) {
-			return true;
-		}
+	} else if (data->hasImage()) {
+		return true;
 	}
 	return false;
 }

Telegram/SourceFiles/boxes/edit_caption_box.cpp
@@ -548,14 +548,9 @@ void EditCaptionBox::prepare() {
 		if (action == Ui::InputField::MimeAction::Check) {
 			if (!data->hasText() && !_isAllowedEditMedia) {
 				return false;
-			}
-			if (data->hasImage()) {
-				const auto image = qvariant_cast<QImage>(data->imageData());
-				if (!image.isNull()) {
-					return true;
-				}
-			}
-			if (const auto urls = data->urls(); !urls.empty()) {
+			} else if (data->hasImage()) {
+				return true;
+			} else if (const auto urls = data->urls(); !urls.empty()) {
 				if (ranges::find_if(
 					urls,
 					[](const QUrl &url) { return !url.isLocalFile(); }

0xmicrosoft15/messenger-desktop-client

commit 7151d80c9659d2aa1faf556c865ac1fa0c370323

Fix pasting of an image with attached URL data.
Regression was introduced in db5d599052.

Fixes #7794.

Telegram/SourceFiles/history/history_widget.cpp
@@ -4467,6 +4467,8 @@ bool HistoryWidget::confirmSendingFiles(
 bool HistoryWidget::canSendFiles(not_null<const QMimeData*> data) const {
 	if (!canWriteMessage()) {
 		return false;
+	} else if (data->hasImage()) {
+		return true;
 	} else if (const auto urls = data->urls(); !urls.empty()) {
 		if (ranges::find_if(
 			urls,
@@ -4474,8 +4476,6 @@ bool HistoryWidget::canSendFiles(not_null<const QMimeData*> data) const {
 		) == urls.end()) {
 			return true;
 		}
-	} else if (data->hasImage()) {
-		return true;
 	}
 	return false;
 }