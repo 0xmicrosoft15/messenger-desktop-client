telegramdesktop/tdesktop

commit cb8f49aea0abeaa037cb736cb795c2677fbf0a0a

Removed Q_OBJECT from ScrollArea.

Telegram/SourceFiles/dialogs/dialogs_widget.cpp
@@ -234,7 +234,10 @@ Widget::Widget(
 		}
 	}, lifetime());
 
-	connect(_scroll, SIGNAL(geometryChanged()), _inner, SLOT(onParentGeometryChanged()));
+	_scroll->geometryChanged(
+	) | rpl::start_with_next(crl::guard(_inner, [=] {
+		_inner->onParentGeometryChanged();
+	}), lifetime());
 	connect(_scroll, SIGNAL(scrolled()), this, SLOT(onListScroll()));
 
 	session().data().chatsListChanges(

Telegram/SourceFiles/chat_helpers/field_autocomplete.cpp
@@ -168,11 +168,10 @@ FieldAutocomplete::FieldAutocomplete(
 
 	hide();
 
-	connect(
-		_scroll,
-		&Ui::ScrollArea::geometryChanged,
-		_inner,
-		&Inner::onParentGeometryChanged);
+	_scroll->geometryChanged(
+	) | rpl::start_with_next(crl::guard(_inner, [=] {
+		_inner->onParentGeometryChanged();
+	}), lifetime());
 }
 
 not_null<Window::SessionController*> FieldAutocomplete::controller() const {

Telegram/SourceFiles/dialogs/dialogs_widget.h
@@ -15,6 +15,8 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "mtproto/sender.h"
 #include "api/api_single_message_search.h"
 
+#include <QtCore/QTimer>
+
 namespace MTP {
 class Error;
 } // namespace MTP

Telegram/SourceFiles/history/admin_log/history_admin_log_section.cpp
@@ -322,8 +322,10 @@ Widget::Widget(
 
 	_scroll->move(0, _fixedBar->height());
 	_scroll->show();
-
-	connect(_scroll, &Ui::ScrollArea::scrolled, this, [this] { onScroll(); });
+	_scroll->scrolls(
+	) | rpl::start_with_next([=] {
+		onScroll();
+	}, lifetime());
 
 	_whatIsThis->setClickedCallback([=] {
 		controller->show(Box<InformBox>(channel->isMegagroup()

Telegram/SourceFiles/history/history_widget.cpp
@@ -252,9 +252,14 @@ HistoryWidget::HistoryWidget(
 		update();
 	}, lifetime());
 
-	connect(_scroll, &Ui::ScrollArea::scrolled, [=] {
+	_scroll->scrolls(
+	) | rpl::start_with_next([=] {
 		handleScroll();
-	});
+	}, lifetime());
+	_scroll->geometryChanged(
+	) | rpl::start_with_next(crl::guard(_list, [=] {
+		_list->onParentGeometryChanged();
+	}), lifetime());
 	_historyDown->addClickHandler([=] { historyDownClicked(); });
 	_unreadMentions->addClickHandler([=] { showNextUnreadMention(); });
 	_fieldBarCancel->addClickHandler([=] { cancelFieldAreaState(); });
@@ -2132,8 +2137,6 @@ void HistoryWidget::showHistory(
 		_send->finishAnimating();
 
 		updateControlsGeometry();
-
-		connect(_scroll, SIGNAL(geometryChanged()), _list, SLOT(onParentGeometryChanged()));
 
 		if (const auto user = _peer->asUser()) {
 			if (const auto &info = user->botInfo) {

Telegram/SourceFiles/history/view/history_view_pinned_section.cpp
@@ -157,7 +157,10 @@ PinnedWidget::PinnedWidget(
 		static_cast<ListDelegate*>(this)));
 	_scroll->move(0, _topBar->height());
 	_scroll->show();
-	connect(_scroll.get(), &Ui::ScrollArea::scrolled, [=] { onScroll(); });
+	_scroll->scrolls(
+	) | rpl::start_with_next([=] {
+		onScroll();
+	}, lifetime());
 
 	setupClearButton();
 	setupScrollDownButton();

Telegram/SourceFiles/history/view/history_view_replies_section.cpp
@@ -224,7 +224,10 @@ RepliesWidget::RepliesWidget(
 		static_cast<ListDelegate*>(this)));
 	_scroll->move(0, _topBar->height());
 	_scroll->show();
-	connect(_scroll.get(), &Ui::ScrollArea::scrolled, [=] { onScroll(); });
+	_scroll->scrolls(
+	) | rpl::start_with_next([=] {
+		onScroll();
+	}, lifetime());
 
 	_inner->editMessageRequested(
 	) | rpl::start_with_next([=](auto fullId) {

Telegram/SourceFiles/history/view/history_view_scheduled_section.cpp
@@ -155,7 +155,10 @@ ScheduledWidget::ScheduledWidget(
 		static_cast<ListDelegate*>(this)));
 	_scroll->move(0, _topBar->height());
 	_scroll->show();
-	connect(_scroll, &Ui::ScrollArea::scrolled, [=] { onScroll(); });
+	_scroll->scrolls(
+	) | rpl::start_with_next([=] {
+		onScroll();
+	}, lifetime());
 
 	_inner->editMessageRequested(
 	) | rpl::start_with_next([=](auto fullId) {

0xmicrosoft15/messenger-desktop-client

commit 0d6bba5e91144be5913be0882e8d5c4078febe22

Fixed scroll in Dialogs::Widget.
Regression was introduced in cb8f49aea0.

Telegram/SourceFiles/dialogs/dialogs_widget.cpp
@@ -205,7 +205,11 @@ Widget::Widget(
 	) | rpl::to_empty);
 
 	connect(_inner, SIGNAL(draggingScrollDelta(int)), this, SLOT(onDraggingScrollDelta(int)));
-	connect(_inner, SIGNAL(mustScrollTo(int,int)), _scroll, SLOT(scrollToY(int,int)));
+	connect(_inner, &InnerWidget::mustScrollTo, [=](int top, int bottom) {
+		if (_scroll) {
+			_scroll->scrollToY(top, bottom);
+		}
+	});
 	connect(_inner, SIGNAL(dialogMoved(int,int)), this, SLOT(onDialogMoved(int,int)));
 	connect(_inner, SIGNAL(searchMessages()), this, SLOT(onNeedSearchMessages()));
 	connect(_inner, SIGNAL(completeHashtag(QString)), this, SLOT(onCompleteHashtag(QString)));
@@ -238,7 +242,10 @@ Widget::Widget(
 	) | rpl::start_with_next(crl::guard(_inner, [=] {
 		_inner->onParentGeometryChanged();
 	}), lifetime());
-	connect(_scroll, SIGNAL(scrolled()), this, SLOT(onListScroll()));
+	_scroll->scrolls(
+	) | rpl::start_with_next([=] {
+		onListScroll();
+	}, lifetime());
 
 	session().data().chatsListChanges(
 	) | rpl::filter([=](Data::Folder *folder) {