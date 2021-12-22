telegramdesktop/tdesktop

commit 5e7e7eaa83a7c211285cee163802466af6ed4505

FullMsgId rely on PeerId instead of ChannelId.

Telegram/SourceFiles/info/media/info_media_list_widget.cpp
@@ -61,7 +61,7 @@ constexpr auto kPreloadedScreensCountFull
 constexpr auto kMediaCountForSearch = 10;
 
 UniversalMsgId GetUniversalId(FullMsgId itemId) {
-	return (itemId.channel != 0)
+	return peerIsChannel(itemId.peer)
 		? UniversalMsgId(itemId.msg)
 		: UniversalMsgId(itemId.msg - ServerMaxMsgId);
 }
@@ -853,10 +853,11 @@ void ListWidget::itemRemoved(not_null<const HistoryItem*> item) {
 FullMsgId ListWidget::computeFullId(
 		UniversalMsgId universalId) const {
 	Expects(universalId != 0);
+	Expects(universalId > 0 || _migrated != nullptr);
 
 	return (universalId > 0)
-		? FullMsgId(peerToChannel(_peer->id), universalId)
-		: FullMsgId(NoChannel, ServerMaxMsgId + universalId);
+		? FullMsgId(_peer->id, universalId)
+		: FullMsgId(_migrated->id, ServerMaxMsgId + universalId);
 }
 
 auto ListWidget::collectSelectedItems() const -> SelectedItems {
@@ -964,9 +965,8 @@ bool ListWidget::isMyItem(not_null<const HistoryItem*> item) const {
 }
 
 bool ListWidget::isPossiblyMyId(FullMsgId fullId) const {
-	return fullId.channel
-		? (_peer->isChannel() && peerToChannel(_peer->id) == fullId.channel)
-		: (!_peer->isChannel() || _migrated);
+	return (fullId.peer == _peer->id)
+		|| (_migrated && fullId.peer == _migrated->id);
 }
 
 bool ListWidget::isItemLayout(

0xmicrosoft15/messenger-desktop-client

commit 0e4873e38a32ad21fd45bbe117bcc5066283130c

Fix assertion violation in media overview.
Regression was introduced in 5e7e7eaa83.

Telegram/SourceFiles/info/media/info_media_list_widget.cpp
@@ -853,11 +853,12 @@ void ListWidget::itemRemoved(not_null<const HistoryItem*> item) {
 FullMsgId ListWidget::computeFullId(
 		UniversalMsgId universalId) const {
 	Expects(universalId != 0);
-	Expects(universalId > 0 || _migrated != nullptr);
 
 	return (universalId > 0)
 		? FullMsgId(_peer->id, universalId)
-		: FullMsgId(_migrated->id, ServerMaxMsgId + universalId);
+		: FullMsgId(
+			(_peer ? _peer.get() : _migrated)->id,
+			ServerMaxMsgId + universalId);
 }
 
 auto ListWidget::collectSelectedItems() const -> SelectedItems {